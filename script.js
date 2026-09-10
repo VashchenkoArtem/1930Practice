2994bd73c47c8dd6bb2994ac09d978606b77fd48
Artem <artemvaschenko83@gmail.com>
2026-09-10 20:23:42 +0300

    Create project

---
+++ script.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const HOST = 'localhost'
+const PORT = 8000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).json("Hello World")
+})
+
+app.listen(PORT, HOST, () => {
+    console.log(`Listening on http://${HOST}:${PORT}`)
+})
