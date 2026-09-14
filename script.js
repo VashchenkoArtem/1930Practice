6b6462624ceb5b9c047fb6df7fd8ce41961b194b
BohdanNovikov0207 <bogdanblacktop@gmail.com>
2026-09-14 18:32:20 +0300

    Hw 14.09

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const moment = require("moment")
 
 const HOST = 'localhost'
 const PORT = 8000
@@ -13,6 +14,25 @@ app.get('/Bohdan', (req, res) => {
     res.status(200).json("Bohdan Novikov")
 })
 
+app.get('/health', (req, res) => {
+    res.status(200).json(
+        {
+            "status": "ok",
+        }
+    )
+})
+
+app.get('/stats', (req, res) => {
+    res.status(200).json(
+        {
+            "uptime": process.uptime(),
+            "nodeVersion": process.version,
+            "timestamp": moment().format("YYYY-MM-DD"),
+        }
+    )
+})
+
 app.listen(PORT, HOST, () => {
     console.log(`Listening on http://${HOST}:${PORT}`)
 })
+
